9f7db4e666cc0a496c4959517846a168e789e4af
Caden W Tucker <ctucker02@ccs.local>
2019-09-24 10:20:24 -0400

    main

--- main.py
+++ main.py
@@ -1,25 +1,39 @@
 import time
 import board
-import adafruit_hcsr04
-sonar = adafruit_hcsr04.HCSR04(trigger_pin=board.D10, echo_pin=board.D9)
+import neopixel
+from adafruit_hcsr04 import HCSR04
+trig = board.D9
+echo = board.D10
+sonar = HCSR04(trig, echo)
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+red = 225
+green = 0
+blue = 0
+
+distance =0
 
 while True:
-    try:
-        print((sonar.distance,)
-        if distance = 20:
-            red = 255
-            green = 0
-            blue = 0
-        if distance < 20:
-            red = 255
-            green = 0
-            blue = 0
-        if distance < 5:
-            red = 255
-            green = 0
-            blue = 0
+    dot.fill((red, green, blue))
+    if distance<=20:
+       red = 255-(12*distance)
+       blue = 0+(12*distance)
+       green = 0
+    elif distance >20:
+        green = 0+(12*(distance-20))
+        blue = 225-(12*(distance-20))
+        red = 0
+    if red<=0:
+        red =0
+    if blue >=225:
+        blue =225
+    if blue <=0:
+        blue = 0
+    if green >=225:
+        green = 225
 
+    try:
+        print (int(sonar.distance))
+        distance = (int(sonar.distance))
     except RuntimeError:
-            print("Retrying!")
-            pass
-            time.sleep(0.1)
+        pass
+    time.sleep(0.05)
