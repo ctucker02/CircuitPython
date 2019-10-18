46bc07a61be4e17f22d241974e9a691d0455ed55
ctucker02 <55394891+ctucker02@users.noreply.github.com>
2019-10-18 09:58:20 -0400

    Update Photo_Interrupter.py

--- Photo_Interrupter.py
+++ Photo_Interrupter.py
@@ -1,30 +1,28 @@
 import time
-import board
-
-
+import board        #these libraries are needed
 from digitalio import DigitalInOut, Direction, Pull
 from lcd.lcd import LCD
 from lcd.i2c_pcf8574_interface import I2CPCF8574Interface
 
 
-lcd = LCD(I2CPCF8574Interface(0x27), num_rows=2, num_cols=16)
+lcd = LCD(I2CPCF8574Interface(0x27), num_rows=2, num_cols=16)  
 PI = DigitalInOut(board.D5)
-PI.direction = Direction.INPUT
+PI.direction = Direction.INPUT  #making photointerrupter pin and declaring as DigitalInOut object
 PI.pull = Pull.UP
 
 
-x = 0
+x = 0   #Set at zero until proven otherwise
 
 interrupt = False
 while True:
     #print(PI.value)
     lcd.backlight = True
-    if PI.value:
+    if PI.value:        
         if interrupt == False:
-            interrupt = True
-            x = x + 1
+            interrupt = True        #if interrupt isn't false then it is true
+            x = x + 1       #count up by one if PI value 
     if not PI.value:
-        interrupt = False
+        interrupt = False   #if not PI value then it is back to starting x=0
         x = x
 
     #print(("Interrupted"))
