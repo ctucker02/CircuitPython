03807bb7db2cf00f40a1fca83325c408f261cfaf
ctucker02 <55394891+ctucker02@users.noreply.github.com>
2019-10-18 09:53:06 -0400

    Update Led_fade.py

--- Led_fade.py
+++ Led_fade.py
@@ -1,16 +1,16 @@
 import time
-import board
+import board    #these libraries are needed
 import pulseio
 
-led = pulseio.PWMOut(board.D13, frequency=5000, duty_cycle=0)
+led = pulseio.PWMOut(board.D13, frequency=5000, duty_cycle=0) #PMW output at D13
 
 while True:
-    for i in range(100):
+    for i in range(100):       #i in range from 0-100
 
         if i < 50:
-            led.duty_cycle = int(i * 2 * 65535 / 100)
+            led.duty_cycle = int(i * 2 * 65535 / 100) #fading up by 65535 / 100
         else:
-            led.duty_cycle = 65535 - int((i - 50) * 2 * 65535 / 100)
+            led.duty_cycle = 65535 - int((i - 50) * 2 * 65535 / 100) #fading down * 65535 / 100
         time.sleep(0.01)
 
 
