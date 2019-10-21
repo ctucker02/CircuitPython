9da4df8ca63a92840e156001ff486a229d8c17d0
ctucker02 <55394891+ctucker02@users.noreply.github.com>
2019-10-21 09:35:14 -0400

    Update distance_sensor.py

--- distance_sensor.py
+++ distance_sensor.py
@@ -1,27 +1,28 @@
 import time
 import board
-import neopixel
-from adafruit_hcsr04 import HCSR04
-trig = board.D9
-echo = board.D10
-sonar = HCSR04(trig, echo)
-dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
-red = 225
-green = 0
-blue = 0
+import neopixel     #necessary libraries
+from adafruit_hcsr04 import HCSR04  
 
-distance =0
+trig = board.D9     
+echo = board.D10    #where the sensor is plugged into
+sonar = HCSR04(trig, echo)      
+dot = neopixel.NeoPixel(board.NEOPIXEL, 1)
+red = 225   #starts at 225
+green = 0   #starts at 0
+blue = 0    #starts at 0
+
+distance = 0 #distance variable starts at 0
 
 while True:
     dot.fill((red, green, blue))
-    if distance<=20:
-       red = 255-(12*distance)
-       blue = 0+(12*distance)
-       green = 0
-    elif distance >20:
-        green = 0+(12*(distance-20))
-        blue = 225-(12*(distance-20))
-        red = 0
+    if distance<=20:            #distance is less than or equal to 20
+       red = 255-(12*distance)  #red decreases by increments of 12 based on the distance
+       blue = 0+(12*distance)   #blue increases by increments of 12 based on the distance
+       green = 0                #green stays the same
+    elif distance >20:                  #distance is greater than or equal to 20
+        green = 0+(12*(distance-20))    #green increases by increments of 12 based on the distance-20
+        blue = 225-(12*(distance-20))   #blue decreases by increments of 12 based on the distance-20
+        red = 0                         #red stays the same
     if red<=0:
         red =0
     if blue >=225:
@@ -34,6 +35,6 @@ while True:
     try:
         print (int(sonar.distance))
         distance = (int(sonar.distance))
-    except RuntimeError:
-        pass
+    except RuntimeError:                    
+        pass                #code won't stop if you are too far away because runtime error is passed
     time.sleep(0.05)
