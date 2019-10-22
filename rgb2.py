8479b6d93903b735ac0f01d87d12057a583fee93
ctucker02 <55394891+ctucker02@users.noreply.github.com>
2019-10-22 09:34:49 -0400

    Update rgb2.py

--- rgb2.py
+++ rgb2.py
@@ -5,14 +5,14 @@ from rgb import RGB   # import the RGB class from the rgb module
 r1 = board.D10
 g1 = board.D9
 b1 = board.D8
-r2 = board.D4
+r2 = board.D4           #defining color pins
 g2 = board.D3
 b2 = board.D5
 
 myRGB1 = RGB(r1,g1,b1)   # create a new RGB object
-myRGB2 = RGB(r2,g2,b2)
+myRGB2 = RGB(r2,g2,b2)  
 
-while True:
+while True:             #colors to run through in order
         myRGB1.red()
         myRGB2.green()
         time.sleep(1)
