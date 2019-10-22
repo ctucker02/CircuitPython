3a5ab811602f839490af5c9c8f8892e98970c2c3
ctucker02 <55394891+ctucker02@users.noreply.github.com>
2019-10-22 09:38:02 -0400

    Update rgb.py

--- rgb.py
+++ rgb.py
@@ -2,26 +2,26 @@ class RGB:
 
     import time
     import digitalio
-    import pulseio      #necessary modules
+    import pulseio      #necessary libraries
 
-    def __init__(self, red, green, blue):
+    def __init__(self, red, green, blue):                                           #defining colors and their duty cycles
         self.mred = self.pulseio.PWMOut(red, frequency=5000, duty_cycle=65535)
         self.mgreen = self.pulseio.PWMOut(green, frequency=5000, duty_cycle=65535)
         self.mblue = self.pulseio.PWMOut(blue, frequency=5000, duty_cycle=65535)
 
     def red(self):
         self.mgreen.duty_cycle = 65535
-        self.mblue.duty_cycle = 65535
+        self.mblue.duty_cycle = 65535   #defines red
         self.mred.duty_cycle = 0
 
     def green(self):
         self.mred.duty_cycle = 65535
-        self.mblue.duty_cycle = 65535
+        self.mblue.duty_cycle = 65535   #defines green
         self.mgreen.duty_cycle = 0
 
     def blue(self):
         self.mgreen.duty_cycle = 65535
-        self.mred.duty_cycle = 65535
+        self.mred.duty_cycle = 65535    #defines blue
         self.mblue.duty_cycle = 0
 
     def cyan(self):
